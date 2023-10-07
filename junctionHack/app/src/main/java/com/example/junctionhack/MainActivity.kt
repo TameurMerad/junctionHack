40bba1d663fde4d1d7e60090cd5ad7e88b8191ec
Soufyan <tameur.merad@gmail.com>
2023-10-07 01:42:40 +0100

    git init

---
+++ junctionHack/app/src/main/java/com/example/junctionhack/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.junctionhack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btm = findViewById<BottomNavigationView>(R.id.btm)
+        replaceFragment(coursesFragment())
+        btm.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.miCourses -> replaceFragment(coursesFragment())
+                R.id.miLeader -> replaceFragment(rankFragment())
+                R.id.miUser -> replaceFragment(profileFragment())
+                else -> {}
+            }
+            true
+        }
+
+
+    }
+
+    private fun replaceFragment (fragment: Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.flfrags , fragment).commit()
+    }
+}
---
+++ junctionHack/app/src/main/java/com/example/junctionhack/rankFragment.kt
@@ -0,0 +1,32 @@
+package com.example.junctionhack
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class rankFragment : Fragment() {
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view =  inflater.inflate(R.layout.fragment_rank, container, false)
+
+
+
+
+
+
+
+
+
+    return view
+    }
+
+
+    }
